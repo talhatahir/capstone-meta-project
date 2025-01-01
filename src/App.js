580dbbe5bded96fbbcf46bb914c08dc629c5c98f
Talha Tahir <trinitro619@gmail.com>
2025-01-01 14:57:15 +0500

    add meta tags

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import "./App.css";
 
-// We'll create these components next
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Menu from './pages/Menu';
-import Reservations from './pages/Reservations';
-import OrderOnline from './pages/OrderOnline';
-import Login from './pages/Login';
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Reservations from "./pages/Reservations";
+import OrderOnline from "./pages/OrderOnline";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <main>
+        <Helmet>
+          <title>Little Lemon Restaurant</title>
+          <meta
+            name="description"
+            content="Mediterranean restaurant offering traditional recipes with a modern twist"
+          />
+          <meta name="og:title" content="Little Lemon Restaurant" />
+          <meta
+            name="og:description"
+            content="Mediterranean restaurant offering traditional recipes with a modern twist"
+          />
+          <meta name="og:image" content="%PUBLIC_URL%/restaurant.jpg" />
+        </Helmet>
         <Nav />
         <div className="page-container">
           <Routes>
