b98d92bcd5ae1aaefb1e71e2c83a1104675c55e5
Talha Tahir <trinitro619@gmail.com>
2025-01-01 18:32:17 +0500

    update setuptest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,10 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
-
-// export const renderWithRouter = (ui, { route = "/" } = {}) => {
-//   window.history.pushState({}, "Test page", route);
-
-//   return render(ui, { wrapper: Router });
-// };
